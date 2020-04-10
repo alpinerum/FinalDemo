5d5097e83ecf0ec5d2d2e674010c1b6033e8eb1e
alpinerum <35735448+alpinerum@users.noreply.github.com>
2020-04-10 00:13:35 -0400

    added demo

---
+++ Assets/Destructible Column Asset/Scripts/ColumnBreakScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnBreakScript : MonoBehaviour {
+
+	
+	public GameObject unbrokenColumn;
+	public GameObject brokenColumn;
+
+	//this determines whether the column will be broken or unbroken at the at runtime
+	public Camera fpsCam;
+	public bool isBroken;
+
+
+	void Start()
+	{ 
+		if (isBroken) {
+			BreakColumn ();
+		} else {
+			unbrokenColumn.SetActive (true);
+			brokenColumn.SetActive (false);
+		}
+	}
+
+
+	void BreakColumn()
+	{
+		isBroken = true;
+		unbrokenColumn.SetActive (false);
+		brokenColumn.SetActive (true);
+	}
+
+
+	void Update()
+	{
+		RaycastHit hit;
+		//this is a placeholder activation for breaking the column when the space key is pressed
+			
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) {
+            if (hit.transform.name == "Unboken Column" && !isBroken && Input.GetMouseButtonDown(0)) {
+				BreakColumn();
+			}
+		}
+	}
+}
---
+++ Assets/Destructible Column Asset/Scripts/ColumnVariableBreakScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnVariableBreakScript : MonoBehaviour
+{
+    [SerializeField]
+    float healthPoints = 100f; //this value can be changed in the inspector
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<Collider>().isTrigger = false;
+        gameObject.AddComponent<Rigidbody>();
+        this.enabled = false;
+    }
+}
